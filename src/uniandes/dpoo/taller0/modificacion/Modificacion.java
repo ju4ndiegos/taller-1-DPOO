6a8d008bd2b9bd3a9d936b2889980531e638c62d
ju4ndiegos <jd.sanchez2>
2023-08-19 16:11:41 -0500

    Primer commit

---
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,25 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	/**
+	 * Este es el método principal de la aplicación, con el que inicia la ejecución
+	 * de la aplicación
+	 * 
+	 * @param args Parámetros introducidos en la línea de comandos al invocar la
+	 *             aplicación
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+	}
+
+}
